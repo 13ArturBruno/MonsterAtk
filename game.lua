6b6d4289e72d22823d3cf3a59496c408ae0a08dd
pisca <pisca_arrombado@gmail.com>
2018-09-17 19:08:34 -0300

    Atualizando Jogo

--- game.lua
+++ game.lua
@@ -1,203 +1,282 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
-
--- Your code here
-
 
 local composer = require( "composer" )
 
 local scene = composer.newScene()
 
-function scene:create( event )
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
 
-	--requires
-
-	local physics = require "physics"
-	physics.start( )
-
-	physics.setGravity( 0, 0 )
-
-	display.setStatusBar( display.HiddenStatusBar )
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
 
 
+-- Initialize variables
+local score = 0
+local lives = 3
+local died = false
+local monstersTable = {}
+local gameLoopTimer
+local scoreText
+local livesText
 
-	--background
-
-	local monstersTable = {}
-	local gameLoopTimer
-
-	local function updateText()
-    	scoreText.text = "Score: " .. score
-	end
-
-	local city1 = display.newImage("city1.png")
-		city1.x = 525
-		city1.y = display.contentCenterY
-		city1.width = 1390
-		city1.height = 770
+local backGroup
+local mainGroup
+local uiGroup
 
 
-	local city2 = display.newImage("city2.png")
-		city2.x = 1915
-		city2.y = display.contentCenterY
-		city2.width = 1394
-		city2.height = 770
+local function updateText()
+	livesText.text = "Lives: " .. lives
+	scoreText.text = "Score: " .. score
+end
 
 
-	local function scrollCity( self, event )
-		if  self.x < -860 then
-			self.x = 1905
-		else 
-			self.x = self.x - 3
-		end
-	end
+local function createMonsters()
 
-	city1.enterFrame = scrollCity
-	Runtime:addEventListener("enterFrame",city1)
-
-	city2.enterFrame = scrollCity
-	Runtime:addEventListener("enterFrame",city2)
-
-
-	--ship
-
-	local ship = display.newImage("ship.png", 200, 200)
-	ship.x = -70
-	ship.y = display.contentCenterY
-	physics.addBody( ship, "dinamic", {density=.1, bounce=0.1, friction=.2, radius=12} )
-
-	--local buttonUp = display.newImage( "control/butup.png")
-	--buttonUp.width = 140
-	--buttonUp.height = 80
-	--buttonUp.x = 100
---	buttonUp.y = display.contentCenterY + 180
-	--buttonUp.alpha = .6
-
-	--local buttonDown = display.newImage( "control/butdown.png")
-	--buttonDown.width = 140
---	buttonDown.height = 80
---	buttonDown.x = 120
---	buttonDown.y = display.contentCenterY + 300
---	buttonDown.alpha = .6
-
-
-
-	--movement ship
-	local function dragShip( event )
- 	
- 	local ship = event.target
-	local phase = event.phase
-
-	if ( "began" == phase ) then
-        display.currentStage:setFocus( ship )
-        ship.touchOffsetY = event.y - ship.y
-
-    elseif ( "moved" == phase ) then
-        ship.y = event.y - ship.touchOffsetY
-    
-    elseif ( "ended" == phase or "cancelled" == phase ) then
-        display.currentStage:setFocus( nil )
-    
-    end
-
-    return true  -- Prevents touch propagation to underlying objects
-
-	end
-	ship:addEventListener( "touch", dragShip )
-
-
-
-
-	--laser
-	local function Laser()
- 
-    local newLaser = display.newImageRect( "laser.png", 30, 20 )
-    physics.addBody( newLaser, "dynamic", { isSensor=true } )
-    newLaser.isBullet = true
-    newLaser.myName = "laser"
-
-    newLaser.x = ship.x + 21
-    newLaser.y = ship.y
-	
-	transition.to( newLaser, { x = 1240, time=500,
-        onComplete = function() display.remove( newLaser ) end
-    } )
-
-	end
-	ship:addEventListener( "tap", Laser )
-
-
-	--creating monsters
-	local function createMonsters()
-
-		local newMonster = display.newImageRect("veni.png", 150, 150)
+		local newMonster = display.newImageRect("_img/veni.png", 150, 150)
 	   	table.insert( monstersTable, newMonster)
 	   	physics.addBody( newMonster, "dynamic", { radius=70, bounce=0.8 } )
 	   	newMonster.myName = "monster"
 
 	   	local whereFrom = math.random(1)
 
-		    if ( whereFrom == 51 ) then
-		        	newMonster.x = 400
-	       			newMonster.y = math.random( 500 )
-	                newMonster:setLinearVelocity( math.random( 40,120 ), math.random( -20,60 ) )
-			elseif ( whereFrom == 2 ) then
-			        newMonster.x = math.random( display.contentWidth )
-			        newMonster.y = -60
-			        newMonster:setLinearVelocity( math.random( -40,40 ), math.random( 40,120 ) )
-			elseif ( whereFrom == 1 ) then
+		    if ( whereFrom == 1 ) then
 			        newMonster.x = display.contentWidth + 300
-			        newMonster.y = math.random(40,740)
+			        newMonster.y = math.random(60,740)
 			        newMonster:setLinearVelocity(-200, 0)
 			end
-	newMonster:applyTorque( math.random( -6,6 ) )
-	end
-
-	local function gameLoop()
-
-		createMonsters()
-	
-	end
-	gameLoopTimer = timer.performWithDelay( 500, gameLoop, 0 )
+end
 
 
-	--colision
-	local function onCollision( event )
+local function Laser()
  
-	    if ( event.phase == "began" ) then
-	 
-	        local obj1 = event.object1
-	        local obj2 = event.object2
+	    local newLaser = display.newImageRect(mainGroup, "_img/laser.png", 30, 20 )
+	    physics.addBody( newLaser, "dynamic", { isSensor=true } )
+	    newLaser.isBullet = true
+	    newLaser.myName = "laser"
+	    newLaser.x = ship.x
+		newLaser.y = ship.y
+		newLaser:toBack()
+		
+		transition.to( newLaser, { x = 1240, time=500,
+	        onComplete = function() display.remove( newLaser ) end
+	    } )
+end
 
-	        if ( ( obj1.myName == "laser" and obj2.myName == "monster" ) or
-	             ( obj1.myName == "monster" and obj2.myName == "laser" ) )
 
-	        then
-	 			-- Remove both the laser and monster
-	            display.remove( obj1 )
-	            display.remove( obj2 )
+local function dragShip( event )
+ 	
+	 	local ship = event.target
+		local phase = event.phase
 
-	        end
+		if ( "began" == phase ) then
+	        display.currentStage:setFocus( ship )
+	        ship.touchOffsetY = event.y - ship.y
+
+	    elseif ( "moved" == phase ) then
+	        ship.y = event.y - ship.touchOffsetY
+	    
+	    elseif ( "ended" == phase or "cancelled" == phase ) then
+	        display.currentStage:setFocus( nil )
+	    
 	    end
-	end
-	Runtime:addEventListener( "collision", onCollision )
+	    return true  -- Prevents touch propagation to underlying objects
+end
+
+
+local function gameLoop()
+
+	createMonsters()
 
 end
+
+
+local function restoreShip()
+
+	ship.isBodyActive = false
+	ship.x = -70
+	ship.y = display.contentCenterY
+
+	-- Fade in the ship
+	transition.to( ship, { alpha=1, time=800,
+		onComplete = function()
+			ship.isBodyActive = true
+			died = false
+		end
+	} )
+end
+
+
+local function endGame()
+	composer.setVariable( "finalScore", score )
+	composer.gotoScene( "highscores", { time=800, effect="crossFade" } )
+end
+
+
+local function onCollision( event )
+
+	if ( event.phase == "began" ) then
+
+		local obj1 = event.object1
+		local obj2 = event.object2
+
+		if ( ( obj1.myName == "laser" and obj2.myName == "monster" ) or
+			 ( obj1.myName == "monster" and obj2.myName == "laser" ) )
+		then
+			-- Remove both the laser and monster
+			display.remove( obj1 )
+			display.remove( obj2 )
+
+			for i = #monstersTable, 1, -1 do
+				if ( monstersTable[i] == obj1 or monstersTable[i] == obj2 ) then
+					table.remove( monstersTable, i )
+					break
+				end
+			end
+
+			-- Increase score
+			score = score + 100
+			scoreText.text = "Score: " .. score
+
+		elseif ( ( obj1.myName == "ship" and obj2.myName == "monster" ) or
+				 ( obj1.myName == "monster" and obj2.myName == "ship" ) )
+		then
+			if ( died == false ) then
+				died = true
+
+				-- Update lives
+				lives = lives - 1
+				livesText.text = "Lives: " .. lives
+
+				if ( lives == 0 ) then
+					display.remove( ship )
+					display.remove(monstersTable)
+					display.remove( monsters )
+
+					timer.performWithDelay( 2000, endGame )
+				else
+					ship.alpha = 0
+					timer.performWithDelay( 1000, restoreShip )
+				end
+			end
+		end
+	end
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+
+	local sceneGroup = self.view
+	-- Code here runs when the scene is first created but has not yet appeared on screen
+
+	physics.pause()  -- Temporarily pause the physics engine
+
+	-- Set up display groups
+	backGroup = display.newGroup()  -- Display group for the background image
+	sceneGroup:insert( backGroup )  -- Insert into the scene's view group
+
+	mainGroup = display.newGroup()  -- Display group for the ship, monsters, lasers, etc.
+	sceneGroup:insert( mainGroup )  -- Insert into the scene's view group
+
+	uiGroup = display.newGroup()    -- Display group for UI objects like the score
+	sceneGroup:insert( uiGroup )    -- Insert into the scene's view group
+	
+	-- Load the background
+	local city1 = display.newImage(backGroup, "_img/city1.png")
+		city1.x = 525
+		city1.y = display.contentCenterY
+		city1.width = 1390
+		city1.height = 770
+
+
+	local city2 = display.newImage(backGroup, "_img/city2.png")
+		city2.x = 1915
+		city2.y = display.contentCenterY
+		city2.width = 1394
+		city2.height = 770
+	
+
+	    ship = display.newImageRect(mainGroup, "_img/ship.png", 100, 150)
+		ship.x = -30
+		ship.y = display.contentCenterY
+		physics.addBody( ship, { radius=30, isSensor=true } )
+		ship.myName = "ship"
+
+
+		city1.enterFrame = scrollCity
+		Runtime:addEventListener("enterFrame",city1)
+
+		city2.enterFrame = scrollCity
+		Runtime:addEventListener("enterFrame",city2)
+
+		-- Display lives and score
+		livesText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+		scoreText = display.newText( uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36 )
+
+		ship:addEventListener( "tap", Laser )
+		ship:addEventListener( "touch", dragShip )
+end
+
+
+-- show()
+function scene:show( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+		physics.start()
+		Runtime:addEventListener( "collision", onCollision )
+		gameLoopTimer = timer.performWithDelay( 400, gameLoop, 0 )
+	end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+		Runtime:removeEventListener( "collision", onCollision )
+		physics.pause()
+		composer.removeScene( "game" )
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
 scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
 return scene
-
-
-
-
-
-
-
-
-
-
-
-
-
--- menu.lua
+++ menu.lua
@@ -8,6 +8,15 @@ local scene = composer.newScene()
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
 
+local function gotoGame()
+	composer.gotoScene( "game", { time=800, effect="crossFade" } )
+end
+
+local function gotoHighScores()
+	composer.gotoScene( "highscores", { time=800, effect="crossFade" } )
+end
+
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -15,20 +24,69 @@ local scene = composer.newScene()
 -- create()
 function scene:create( event )
 
+	local sceneGroup = self.view
+	-- Code here runs when the scene is first created but has not yet appeared on screen
 
-	local background = display.newImageRect("menu/bg.png", 1355, 755)
+	local background = display.newImageRect( sceneGroup, "_img/bg.png", 1355, 755)
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 
-	local function gotoGame()
-		composer.gotoScene( "game" )
-	end
+	local title = display.newImageRect( sceneGroup, "_img/title.png", 580, 160 )
+	title.x = display.contentCenterX + 370
+	title.y = 100
 
-	local playButton = display.newImageRect("menu/start.png", 200, 200 )
-	      playButton.x = display.contentCenterX + 200
+	--local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
+	--playButton:setFillColor( 0.82, 0.86, 1 )
+	local playButton = display.newImageRect(sceneGroup, "_img/start.png", 200, 200 )
+	      playButton.x = display.contentCenterX + 150
 	      playButton.y = display.contentCenterY + 200
 
+	local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX + 460, display.contentCenterY + 200, native.systemFont, 60 )
+	highScoresButton:setFillColor( 0.75, 0.78, 1 )
+
 	playButton:addEventListener( "tap", gotoGame )
+	highScoresButton:addEventListener( "tap", gotoHighScores )
+end
+
+
+-- show()
+function scene:show( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+
+	end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+
 end
 
 
@@ -36,9 +94,9 @@ end
 -- Scene event function listeners
 -- -----------------------------------------------------------------------------------
 scene:addEventListener( "create", scene )
---scene:addEventListener( "show", scene )
---scene:addEventListener( "hide", scene )
---scene:addEventListener( "destroy", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
 -- -----------------------------------------------------------------------------------
 
 return scene
